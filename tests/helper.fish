a80fe1fe01d5a967e3105db62b6c2c87921df257
Will Price <will.t.price@boeing.com>
2016-03-25 13:46:39 +0000

    Install fish-tank in travis and read path of FISH_TANK from env var

--- tests/helper.fish
+++ tests/helper.fish
@@ -1,5 +1,4 @@
-set -l fish_tank /usr/local/share/fish-tank/tank.fish
-if not test -e $fish_tank
+if not test -e "$FISH_TANK"
   echo 'error: fish-tank is required to run these tests (https://github.com/terlar/fish-tank)'
   exit 1
 end
@@ -10,6 +9,6 @@ function assert_status
 end
 
 set -l current_directory (dirname (status -f))
-source "$fish_tank"
+source "$FISH_TANK/tank.fish"
 source "$current_directory"/../_omf_plugin_proxy_*.fish
 source "$current_directory"/../{,no}proxy.fish
